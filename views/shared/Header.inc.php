6878a542a38d61ae27adc8be78d81d8123086557
Danyella <danyellastrikann@duck.com>
2024-06-06 17:31:14 +0200

    feat(backend): :sparkles: add style change to header

--- views/shared/Header.inc.php
+++ views/shared/Header.inc.php
@@ -1,13 +1,24 @@
-<header >
+<?php
+    function is_current_page(string $pageName) : bool {
+        $current_dir = $GLOBALS['current_dir'];
+        foreach (explode('+', $current_dir) as $str) {
+            if ($pageName === $str) return true;
+        }
+
+        return false;
+    }
+?>
+
+<header>
     <nav style="display:flex; justify-content: space-between">
         <div class="left">
-            <a href="/">Accueil</a>
-            <a href="#">Jardin potager</a>
-            <a href="#">Contact</a>
+            <a href="/" <?= is_current_page('home') ? 'aria-current="page"' : '' ?>>Accueil</a>
+            <a href="#" <?= is_current_page('garden') ? 'aria-current="page"' : '' ?>>Jardin potager</a>
+            <a href="#" <?= is_current_page('contact') ? 'aria-current="page"' : '' ?>>Contact</a>
         </div>
         <div class="right">
-            <a href="#">S'identifier</a>
-            <a href="#">Se connecter</a>
+            <a href="/inscription" <?= is_current_page('register') ? 'aria-current="page"' : '' ?>>S'inscrire</a>
+            <a href="/login" <?= is_current_page('login') ? 'aria-current="page"' : '' ?>>Se connecter</a>
         </div>
     </nav>
 </header>
