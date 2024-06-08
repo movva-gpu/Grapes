6fa58116795bef76962e7a887f6c03b18278dc75
Danyella <danyellastrikann@duck.com>
2024-06-08 13:56:44 +0200

    style(backend): :technologist: amélioration de la lisibilité de la fonction getAsset de dix milliards pourcents

--- conf/utils.inc.php
+++ conf/utils.inc.php
@@ -36,24 +36,23 @@ function viewsPath(string $path): string
 function getAsset(string $path): string
 {
     $fileAssetsPath = 'assets/' . $path;
-    $fileAssetsPathExists = file_exists(rootPath($fileAssetsPath));
 
     $fileDistPath = 'dist/' . $path;
-    $fileDistPathExists = file_exists(rootPath($fileDistPath));
 
-    $fileExtension = explode('.', $path)[1];
-    $fileNameOrPath = explode('.', $path)[0];
+    $fileExtension = pathinfo($path, PATHINFO_EXTENSION);
+    $fileNameOrPath = pathinfo($path, PATHINFO_FILENAME);
     $minifiedFilename = $fileNameOrPath . '.min.' . $fileExtension;
     $minifiedFilePath = 'dist/' . $minifiedFilename;
-    $minifiedFilePathExists = file_exists(rootPath($minifiedFilePath));
 
-    return $minifiedFilePathExists
-        ? $minifiedFilePath
-        : ($fileDistPathExists
-            ? $fileDistPath
-            : ($fileAssetsPathExists
-                ? $fileAssetsPath
-                : throw new InvalidArgumentException('Asset ' . $path . ' not found.', 404)));
+    if (file_exists(rootPath($minifiedFilePath))) {
+        return $minifiedFilePath;
+    } elseif (file_exists(rootPath($fileDistPath))) {
+        return $fileDistPath;
+    } elseif (file_exists(rootPath($fileAssetsPath))) {
+        return $fileAssetsPath;
+    }
+
+    throw new InvalidArgumentException('Asset ' . $path . ' not found.', 404);
 }
 
 enum ErrorTypesEnum
