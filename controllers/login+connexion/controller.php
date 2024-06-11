3350d895bd0a711ecbd4fa00e064edb9784b9240
Danyella <danyellastrikann@duck.com>
2024-06-11 08:42:09 +0200

    wip(backend): ajoute la base de la connection
    je commit pour récupérer mon travail sur linux

---
+++ controllers/login+connexion/controller.php
@@ -0,0 +1,14 @@
+<?php
+
+function index(): void
+{
+    $view_name = 'Login';
+    $page_name = 'Connexion';
+
+    require_once views_path('templates/Main_template.php');
+}
+
+function validate(): void
+{
+
+}
