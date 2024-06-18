9f050a0b198c10fe622de2e8998b6acba3d7a4ad
movva-gpu <danyellastrikann@duck.com>
2024-06-18 12:42:55 +0200

    Pas le temps

--- views/Legal_view.php
+++ views/Legal_view.php
@@ -1,4 +1,5 @@
-Mentions Légales de l'Application Grapes
+<h1>Mentions Légales de l'Application Grapes</h1>
+
 1. Éditeur de l'Application
 Grapes est une application d'éco-jardinage éditée par :
 Nom de l'entreprise : Grapes
@@ -14,5 +15,30 @@ Adresse : 9 Rue Quebec, 10430 Rosières-prés-Troyes
 Téléphone : 06 06 06 06 06
 Email : Blossom.media@gmail.com
 3. Propriété Intellectuelle
-L'ensemble des éléments constituant l'application Grapes (textes, graphiques, logiciels, photographies, illustrations, logos, etc.) sont la propriété exclusive de Grapes , sauf mention contraire explicite.
-Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments de l'application, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Grapes
+L'ensemble des éléments constituant l'application Grapes (textes, graphiques, logiciels, photographies, illustrations,
+logos, etc.) sont la propriété exclusive de Grapes , sauf mention contraire explicite.
+Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments de
+l'application, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Grapes
+
+<ol>
+    <li>Éditeur de l'Application<br>
+        Grapes est une application d'éco-jardinage éditée par :<br>
+        Nom de l'entreprise : Grapes<br>
+        Adresse : 9 Rue Quebec, 10430 Rosières-prés-Troyes<br>
+        Téléphone : 06 06 06 06 06<br>
+        Email : Grapes.co@gmail.com<br>
+        RCS : 123456<br>
+        Directeur de la publication : Mr .Patate</li>
+    <li>Hébergement de l'Application<br>
+        L'application Grapes est hébergée par :<br>
+        Nom de l'hébergeur : BlossomMédia<br>
+        Adresse : 9 Rue Quebec, 10430 Rosières-prés-Troyes<br>
+        Téléphone : 06 06 06 06 06<br>
+        Email : Blossom.media@gmail.com</li>
+    <li>Propriété Intellectuelle<br>
+        L'ensemble des éléments constituant l'application Grapes (textes, graphiques, logiciels, photographies,
+        illustrations, logos, etc.) sont la propriété exclusive de Grapes , sauf mention contraire explicite.
+        Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments de
+        l'application, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable
+        de Grapes</li>
+</ol>
